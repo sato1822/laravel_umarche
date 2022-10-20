ed476bed7151c2ec6ebcfadffaf915d78678e94b
iori sato <iori1822@icloud.com>
2022-10-20 18:09:15 +0900

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use App\Models\Shop;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShopController extends Controller
+{
+    //
+    public function __construct()
+    {
+      $this->middleware('auth:owners');
+
+      $this->middleware(function ($request, $next) {
+      // dd($request->route()->parameter('shop')); //文字列
+      // dd(Auth::id()); //数字
+
+        $id = $request->route()->parameter('shop');//shopのIdの取得(現在文字列での取得状態) 
+        if(!is_null($id)){//空でなかったら
+          $shopsOwnerId = Shop::FindOrFail($id)->owner->id;//オーナーのIdの取得
+          $shopId = (int)$shopsOwnerId;//文字列から数値に変換
+          $ownerId = Auth::id();
+          if($shopId !== $ownerId){//同じでなかったら
+            abort(404);//404 非表示画面の表示
+          }
+        }
+        return $next($request);
+    });
+    }
+
+    public function index()
+    {
+      $ownerId = Auth::id();
+      $shops = Shop::where('owner_id', $ownerId)->get();
+
+      return view('owner.shops.index', compact('shops'));
+    }
+
+    public function edit($id)
+    {
+      dd(Shop::FindOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+}
