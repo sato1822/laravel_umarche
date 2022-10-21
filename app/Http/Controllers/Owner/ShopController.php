292d734b2bb2809d68da83887d7ceec5d0ad8681
iori sato <iori1822@icloud.com>
2022-10-21 23:37:33 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -49,7 +50,7 @@ class ShopController extends Controller
       return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)//requestで指定してバリデーションが使える
     {
       $imageFile = $request->image;
       if(!is_null($imageFile) && $imageFile->isValid()){
