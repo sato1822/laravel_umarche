cbba6fa5c417d923570478267c699e16da41a0b7
iori sato <iori1822@icloud.com>
2022-10-26 10:15:59 +0900

    🇱store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -7,12 +7,20 @@ use InterventionImage;
 
 class ImageService
 {
-  public static function upload($imageFile, $folderName)
+  public static function upload($imageFile, $folderName)//staticとすることで他のクラスでもつかうことができる
   {
+    // dd($imageFile['image']);
+    if(is_array($imageFile))
+    {
+      $file = $imageFile['image'];
+    } else{
+      $file = $imageFile;
+    }
+
     $filename = uniqid(rand(), '_');
-    $extension = $imageFile->extension();
+    $extension = $file->extension();
     $fileNameToStore = $filename . '.' . $extension;
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+    $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
     // dd($imageFile, $resizedImage);
 
     Storage::put('public/'. $folderName . '/' . $fileNameToStore, $resizedImage);
