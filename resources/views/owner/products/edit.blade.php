94b2c2ebf785427c1bab024a841177cd1e86fc99
iori sato <iori1822@icloud.com>
2022-11-01 09:16:20 +0900

    product update

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -10,8 +10,10 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                <x-flash-message status="session('status')" />
                 <form method="post" action="{{ route('owner.products.update', ['product' => $product->id])}}" >
                   @csrf
+                  @method('put')
                     <div class="-m-2">
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
@@ -40,8 +42,8 @@
                       </div>
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
-                          <label for="quantity" class="leading-7 text-sm text-gray-600">現在の在庫 ※必須</label>
-                          <input type="hidden" id="quantity" name="quantity" value="{{ $quantity}}">
+                          <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫 ※必須</label>
+                          <input type="hidden" id="current_quantity" name="current_quantity" value="{{ $quantity}}">
                           <div class="w-full bg-gray-100 bg-opacity-50 rounded text-base outline-none text-gray-700 py-1 px-3 leading-8">{{ $quantity }}</div>
                         </div>
                       </div>
@@ -51,7 +53,7 @@
                             <input type="radio" name="type" value="1" class="mr-2" checked >追加
                           </div>
                           <div> 
-                            <input type="radio" name="type" value="0" class="mr-2" >削減
+                            <input type="radio" name="type" value="2" class="mr-2" >削減
                           </div>
                         </div>
                       </div>
